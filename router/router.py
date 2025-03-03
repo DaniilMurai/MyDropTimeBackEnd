12d53df2e1806d738a3dd2c8bf3af748620440ae
DaniilMurai <daniilmuray@gmail.com>
2025-03-03 18:21:26 +0200

    bug fixing

--- router/router.py
+++ router/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from . import cloudinary_, products
+from . import cloudinary_, products, categories
 
 router = APIRouter()
 
 router.include_router(products.router)
 router.include_router(cloudinary_.router)
+router.include_router(categories.router())
